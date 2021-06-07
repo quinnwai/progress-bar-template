5f93d815fbb24ad13e77c883e5afa6d5cd1820d2
quinnwaiwong <quinnwai.wong@gmail.com>
2021-06-07 17:00:20 -0500

    initial draft

--- src/components/ProgressBar/index.js
+++ src/components/ProgressBar/index.js
@@ -2,21 +2,33 @@ import React from 'react'
 import { Text, View, StyleSheet } from 'react-native'
 
 const ProgressBar = props => {
-  const { color, text } = props
+  const {low, high, color, text, barColor, backgroundColor, borderColor } = props
+  const flex = low/high
 
+  // what actually shows up in app
   return (
-    <View style={styles.wrapper}>
-      <Text style={{ color }}>{text}</Text>
+    <>
+    <View style={[styles.wrapper, {backgroundColor: backgroundColor}, {borderColor: barColor}]}>
+      <View style={[{flex}, {backgroundColor: barColor}]}>
+
+      </View>
     </View>
+    <Text style={ [styles.text, {color}] }>{text}</Text>
+    </>
   )
 }
 
 const styles = StyleSheet.create({
   wrapper: {
-    display: 'flex',
+    flexDirection: 'row',
+    height: 50,
+    padding: 0,
+    borderWidth: 2,
+  },
+  text: {
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 })
 
 export default ProgressBar
