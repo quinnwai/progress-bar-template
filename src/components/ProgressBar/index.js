178fb53830a8b24bdced1291614b5d2641d2b35b
quinnwaiwong <quinnwai.wong@gmail.com>
2021-06-09 10:46:54 -0500

    rounding, orientation, small fixes

--- src/components/ProgressBar/index.js
+++ src/components/ProgressBar/index.js
@@ -1,34 +1,64 @@
 import React from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import { View, StyleSheet } from 'react-native'
+import PropTypes from 'prop-types'
 
 const ProgressBar = props => {
-  const {low, high, color, text, barColor, backgroundColor, borderColor } = props
-  const flex = low/high
+  // object destructuring and calculations for size of bar
+  const {
+    low,
+    high,
+    height,
+    flexDirection,
+    borderRadius,
+    barColor,
+    borderColor,
+    backgroundColor,
+  } = props
 
-  // what actually shows up in app
+  // set flex value (relative length) of bar
+  let flex = low / high
+  if (flex > 1) {
+    flex = 1
+  }
+
+  // combining static and dynamic styles into array (readability)
+  const wrapperStyles = [
+    styles.wrapper,
+    {
+      height: height == null ? 50 : height,
+      backgroundColor: backgroundColor,
+      borderColor: borderColor,
+      borderRadius: borderRadius == null ? 20 : borderRadius,
+      flexDirection: flexDirection == null ? 'row' : flexDirection,
+    },
+  ]
+  const barStyles = {
+    flex: flex == null ? 0.25 : flex,
+    backgroundColor: barColor,
+  }
+
+  // return actual UI shown in the app/editor
   return (
-    <>
-    <View style={[styles.wrapper, {backgroundColor: backgroundColor}, {borderColor: barColor}]}>
-      <View style={[{flex}, {backgroundColor: barColor}]}>
-
-      </View>
+    <View style={wrapperStyles}>
+      <View style={barStyles} />
     </View>
-    <Text style={ [styles.text, {color}] }>{text}</Text>
-    </>
   )
 }
 
+// Typechecking (for magic text in Adalo editor especially)
+ProgressBar.propTypes = {
+  low: PropTypes.number,
+  high: PropTypes.number,
+  height: PropTypes.number,
+  borderRadius: PropTypes.number,
+}
+
+// static styles with defaults
 const styles = StyleSheet.create({
   wrapper: {
-    flexDirection: 'row',
-    height: 50,
-    padding: 0,
+    overflow: 'hidden',
     borderWidth: 2,
   },
-  text: {
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
 })
 
 export default ProgressBar
